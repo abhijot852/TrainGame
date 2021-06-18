f473eb126a10083cd1262dce07c452e81c56e0af
abhijot852 <82717648+abhijot852@users.noreply.github.com>
2021-06-18 22:53:42 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# TrainGame
+Output Link:
+https://deepali-m.github.io/physics-engine-stage-2-final-/index.html
+
+https://deepali-m.github.io/physics-engine-stage-2-final-/
---
+++ Rock.js
@@ -0,0 +1,28 @@
+class Rock{
+    constructor(x,y,width,height){
+      var options ={ isStatic: false}
+      this.body =  Bodies.rectangle(x,y,width,height,options);
+     
+      this.width = width;
+      this.height = height;
+      this.image=loadImage("images/rock1.png");
+      World.add(myWorld,this.body);
+    }
+    show()
+    {
+      var pos = this.body.position;
+
+      //this.body.position.x = mouseX;
+      //this.body.position.y = mouseY;
+        imageMode(CENTER);
+        fill('black');
+        stroke ("grey");
+        image(this.image,pos.x,pos.y,this.width, this.height);
+    }
+    score(){
+      if (this.Visiblity < 0 && this.Visiblity > -1005){
+        score++;
+      }
+    }
+
+}
